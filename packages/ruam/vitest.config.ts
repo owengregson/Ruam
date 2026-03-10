77dbba2d6a2f6251e86566de900425a918723527
owengregson <owen@owen.lol>
2026-03-10 01:05:24 -0700

    indentation and import fixes

--- packages/ruam/vitest.config.ts
+++ packages/ruam/vitest.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    globals: true,
-    testTimeout: 30_000,
-    include: ["test/**/*.test.ts"],
-    reporters: ["default", "json"],
-    outputFile: {
-      json: "test-results.json",
-    },
-    execArgv: ["--experimental-vm-modules"],
-  },
+	test: {
+		globals: true,
+		testTimeout: 30_000,
+		include: ["test/**/*.test.ts"],
+		reporters: ["default", "json"],
+		outputFile: {
+			json: "test-results.json",
+		},
+		execArgv: ["--experimental-vm-modules"],
+	},
 });
